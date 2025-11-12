ae297108a6575576c70d7007463a1b31c0462853
Guntur-Indrasara12 <gunturindrasara11@gmail.com>
2025-11-12 16:20:29 +0700

    fix chapter 1: clear

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,8 +11,8 @@ import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
 import { createInvoice, State } from '@/app/lib/actions';
 
-function createInvoiceAction(prevState: any, formData: FormData) {
-  return createInvoice(formData);
+function createInvoiceAction(prevState: State, formData: FormData) {
+  return createInvoice(prevState, formData);
 }
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
